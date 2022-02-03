df672ec27408ad99a7cfdce8b9e2f67c364a4083
AljoschaT <aljoscha.troester@edu.teko.ch>
2022-02-03 06:56:06 +0100

    Hausaufgaben Tag 2 Aufgabe 1

---
+++ src/ch/teko/prg/tag02/input/Hausaufgaben2/Aufgabe3/Calculat.java
@@ -0,0 +1,19 @@
+package ch.teko.prg.tag02.input.Hausaufgaben2.Aufgabe3;
+
+public class Calculat {
+
+    public static void main(String[] args) {
+        int i = 3;
+        double d = 3.0;
+
+        System.out.println(7 / i + 1.5f);
+        System.out.println(13 % i);
+        System.out.println(10 / i + 3);
+        System.out.println(10 / (d + 1));
+        System.out.println(7 / (i + 1.0));
+        System.out.println(8 + i * d);
+
+
+
+    }
+}
